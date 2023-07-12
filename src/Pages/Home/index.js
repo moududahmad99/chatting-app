93d71043933e1a7452da98be8815105fc86ec0b3
Moudud Ahmad <sabbirahmedmoudud@gmail.com>
2023-07-12 16:37:56 +0600

    Added Build Folder and Upgrade Home Page

--- src/Pages/Home/index.js
+++ src/Pages/Home/index.js
@@ -2,9 +2,11 @@ import React from 'react'
 import './style.css'
 
 const Home = () => {
-  return (
-    <div>This is my Homepage</div>
-  )
+	return (
+		<React.Fragment>
+			<h1 className='temp-class'>I'm Working on this project.</h1>
+		</React.Fragment>
+	)
 }
 
 export default Home;
